f66aa72bb365459e04553723250a4595e95c799c
zMaid <zMaiD@arv.co.th>
2023-04-26 21:25:00 +0700

    change host

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function sendEmail() {
                 "Số điện thoại: " + phone + "\n" +
                 "Ngành đăng ký: " + regisBranch
     const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:3000/send-email');
+    xhr.open('POST', 'https://be-neu-elearning.onrender.com/send-email');
     xhr.setRequestHeader('Content-Type', 'application/json')
 
     xhr.onload = () => {
